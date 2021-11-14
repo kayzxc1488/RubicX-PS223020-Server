e7ce13c6769fc44f3bf4022f49b5ea00fca78811
TitikUniverse <semyon.titov.203@mail.ru>
2021-11-14 17:54:55 +0300

    Сделал сервер и немного базу данных

---
+++ RubicX_PS223020/RubicX_PS223020.DataAccess.Core/Models/UserRto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RubicX_PS223020.DataAccess.Core.Models
+{
+    [Table("User")]
+    public class UserRto
+    {
+        [Key] public int Id { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        [Required] public string PhoneNumberPrefix { get; set; }
+        [Required] public string PhoneNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public DateTimeOffset Birthday { get; set; }
+        public string AvatarUrl { get; set; }
+        public UserRoleRto Role { get; set; }
+    }
+}
