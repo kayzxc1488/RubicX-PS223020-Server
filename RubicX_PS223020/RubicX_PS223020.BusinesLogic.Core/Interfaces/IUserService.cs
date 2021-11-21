2c2fb4dbfcb48f3eb0870687a9c94f306fa0cce9
TitikUniverse <semyon.titov.203@mail.ru>
2021-11-21 17:53:15 +0300

    Доделал БД юзера, начал бизнес логику юзера

---
+++ RubicX_PS223020/RubicX_PS223020.BusinesLogic.Core/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using RubicX_PS223020.BusinesLogic.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubicX_PS223020.BusinesLogic.Core.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserInformationBlo> RegisterWithPhone(string numberPrefix, string number, string password);
+    }
+}
--- RubicX_PS223020/RubicX_PS223020.DataAccess.Core/Models/UserRto.cs
+++ RubicX_PS223020/RubicX_PS223020.DataAccess.Core/Models/UserRto.cs
@@ -10,13 +10,20 @@ namespace RubicX_PS223020.DataAccess.Core.Models
         [Key] public int Id { get; set; }
         public string Email { get; set; }
         public string Login { get; set; }
+        public bool IsBoy { get; set; }
         [Required] public string PhoneNumberPrefix { get; set; }
         [Required] public string PhoneNumber { get; set; }
+        [Required, MinLength(7)] public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Patronymic { get; set; }
         public DateTimeOffset Birthday { get; set; }
         public string AvatarUrl { get; set; }
         public UserRoleRto Role { get; set; }
+        
+        [NotMapped] public string GetFullName
+        {
+            get => FirstName + " " + LastName + " " + Patronymic;
+        }
     }
 }
