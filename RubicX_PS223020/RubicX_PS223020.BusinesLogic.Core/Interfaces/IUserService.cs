df13e051c6ee64eb847eebc70393c6cd98c7ac11
TitikUniverse <semyon.titov.203@mail.ru>
2021-11-28 18:01:00 +0300

    Начали юзер сервис

--- RubicX_PS223020/RubicX_PS223020.BusinesLogic.Core/Interfaces/IUserService.cs
+++ RubicX_PS223020/RubicX_PS223020.BusinesLogic.Core/Interfaces/IUserService.cs
@@ -10,5 +10,11 @@ namespace RubicX_PS223020.BusinesLogic.Core.Interfaces
     public interface IUserService
     {
         Task<UserInformationBlo> RegisterWithPhone(string numberPrefix, string number, string password);
+        Task<UserInformationBlo> AuthWithPhone(string numberPrefix, string number, string password);
+        Task<UserInformationBlo> AuthWithEmail(string email, string password);
+        Task<UserInformationBlo> AuthWithLogin(string login, string password);
+        Task<UserInformationBlo> Get(int userId);
+        Task<UserInformationBlo> Update(string numberPrefix, string number, string password, UserUpdateBlo userUpdateBlo);
+        Task<bool> DoesExist(string numberPrefix, string number);
     }
 }
