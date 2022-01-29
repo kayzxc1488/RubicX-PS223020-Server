bf847863fd4263fac8ead4af8d35a05e788248a9
shortsharf <bobbobie24@gmail.com>
2022-01-29 17:53:04 +0300

    421
    2365

---
+++ RubicX_PS223020/RubicX_PS223020.DataAccess.Core/test/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubicX_PS223020.DataAccess.Core.test
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public int Cost
+
+    }
+}
